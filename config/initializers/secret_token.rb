9a79a7918091ec675a5e5c4298006ad0bef4716c
Rui Cheng <ktei2008@gmail.com>
2013-09-10 15:02:52 +1000

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MoreThanNerds::Application.config.secret_key_base = '4dbaaf3b8a77919a6fb3a41975d6dc415623e4b39b05d00e84cb236cc9fd80c36ccb613c8abb560efc708d2e5415e032dc86f3222b6b1484b4083b7b4b024cdb'
